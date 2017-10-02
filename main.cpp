0ad17026e260618260e79db19d52cd09ad1c25fb
aalqattan <aalqattan@mail.csuchico.edu>
2017-10-02 16:37:34 -0700

    create new file and add commints for the project2

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<victor>
+#include<string>
+#include "bitmap.h"
+
+
+using namespace std;
+
+
+int main()
+
+{
+// decleare variables for the user to store the input. 
+// output to the user to enter the file name of an image. 
+// store the uer's input as a file name. 
+//add the code to convert the image so every pixel in it's greyscale.
+// save the file as oldtimey.bmp
+
+
+
+return 0;
+
+}
