1b9080687eea650c3f0bbac9b9bec926541a90b8
aalqattan <aalqattan@mail.csuchico.edu>
2017-10-13 10:30:29 -0700

    final change

--- main.cpp
+++ main.cpp
@@ -11,80 +11,73 @@ int main()
 
 
 {
+    //declear variables outside the while loop. 
+    bool validBmp;
+    Bitmap image;
+    Pixel rgb;
+    string fileName;
+    vector < vector < Pixel > > bmp;
 
-string fileName;
+    do{
 
-
-cout<<"Welcome to image color changer :)"<<endl;
-cout<<endl;
-cout<<"Please enter the file name which should be windows bmp"<<endl;
-cin>>fileName;
-
-
-
-Bitmap image; //declear photo in Bitmap 
-
-//if (fileName =="machupicchu.bmp")
-//{
-vector <vector < Pixel > > bmp;
+        cout<<"Welcome to image color changer :)"<<endl;
+        cout<<endl;
+        cout<<"Please enter the file name which should be windows bmp"<<endl;
+        cin>>fileName;
 
 
 
-Pixel rgb;
-	
-image.open ("machupicchu.bmp"); //read bmp file and convert it to pixel matrix
+        image.open (fileName); //read bmp file and convert it to pixel matrix.
+        validBmp = image.isImage(); //assign validBmp variable to the bmp image.
+      
 
-bool validBmp = image.isImage(); //verigy that file opened was a valid image
 
-if (validBmp == true)
-        {
+
+        } while (validBmp == false);
+
+
+
+  
         bmp = image.toPixelMatrix(); //convert bmp file to Matrix file. 
 
-	int row = bmp.size();
-	int cols = bmp [0].size();
+    	int row = bmp.size();
+	    int cols = bmp [0].size();
 
  
 	
 	
 		for(int i = 0; i < row; i++) 
-	{
-		for(int c= 0; c < cols; c++)
-       {	  
-	rgb = bmp [i][c];
+     	{
+	    	for(int c= 0; c < cols; c++)
+      	    {  
+	            rgb = bmp[i][c];
 	       
-	 int b = rgb.blue;
-         int g = rgb.green;
-	 int r = rgb.red;
+    	        int b = rgb.blue;
+                int g = rgb.green;
+	            int r = rgb.red;
 
-         int avg=(b + g + r) / 3;
+                int avg=(b + g + r) / 3;
 
 		
-     rgb.red =avg;
-	 rgb.blue =avg;
-	 rgb.green =avg;
+                rgb.red =avg;
+	            rgb.blue =avg;
+	            rgb.green =avg;
 	       
 	
-	  bmp[i][c]= rgb;
-          }
+	            bmp[i][c] = rgb;
+        
+            }  
+
+        }   
+
+
+     
 
 
 
-}
-}
+	 image.fromPixelMatrix(bmp);
+     image.save("oldtimey.bmp");
 
-else {
-	cout<<" Please enter a valid file name"<<endl;
-}
- 
-
-//else {
-  //      cout<<"Please enter a valid file name which is (machupicchu.bmp) since we have only this image"<<endl;
-    // }    
-
-
-
-	  image.fromPixelMatrix(bmp);
-          image.save("oldtimey.bmp");
 
 return 0;
 
